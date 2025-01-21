4315da2c36e4d2e908de023f8b5f2dd8c17e0551
Ethan <yangethan1@gmail.com>
2025-01-20 22:49:33 -0600

    dynamically resized grid works now

--- main.js
+++ main.js
@@ -1,13 +1,33 @@
 const container = document.querySelector(".container");
 
-for (i = 0; i < 16; i++) {
-    for (f = 0; f < 16; f++) {
-        const grid = document.createElement("div");
-        grid.classList.add("grid");
-        // grid.textContent = "Testing";
-        container.appendChild(grid);
+const grid_px_size = 500;
+
+function makeGrid(grid_size) {
+    const width = grid_px_size / grid_size;
+    for (i = 0; i < parseInt(grid_size); i++) {
+        for (f = 0; f < parseInt(grid_size); f++) {
+            const grid = document.createElement("div");
+            grid.classList.add("grid");
+            container.appendChild(grid);
+        }
     }
+    container.style.gridTemplateColumns = `repeat(${grid_size}, ${width}px)`
 }
+
+makeGrid(16);
+
+const button = document.querySelector(".button-main");
+
+button.addEventListener("click", function() {
+    let grid_size = prompt("Please enter a grid size");
+    if (grid_size < 0 || grid_size > 100) {
+        grid_size = 0;
+    };
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    };
+    makeGrid(grid_size);
+});
 
 const grids = document.querySelectorAll(".grid");
 
